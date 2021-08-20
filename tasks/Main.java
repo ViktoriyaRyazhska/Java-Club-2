b5928b77e43460a7b500767274a807a163d27ffc
Yura Yanio <yray422@gmail.com>
2021-08-20 09:58:20 +0300

    Create task7 and modified Main class

--- tasks/Main.java
+++ tasks/Main.java
@@ -15,31 +15,121 @@ public class Main {
 		// Ã‘â€šÃ‘Æ’Ã‘â€š Ã�Â²Ã‘ï¿½Ã‘â€šÃ�Â°Ã�Â²Ã�Â»Ã‘ï¿½Ã‘â€�Ã�Â¼Ã�Â¾ main Ã�Â½Ã�Â°Ã‘Ë†Ã�Â¸Ã‘â€¦ Ã�Â·Ã�Â°Ã�Â´Ã�Â°Ã‘â€¡ Ã‘â€“ Ã�Â±Ã‘Æ’Ã�Â´Ã�ÂµÃ�Â¼Ã�Â¾ Ã�Â²Ã�Â¸Ã�ÂºÃ�Â»Ã�Â¸Ã�ÂºÃ�Â°Ã‘â€šÃ�Â¸ Ã�Â·Ã�Â° Ã�Â´Ã�Â¾Ã�Â¿Ã�Â¾Ã�Â¼Ã�Â¾Ã�Â³Ã�Â¾Ã‘Å½ switch-case
 		// Ã�Â´Ã�Â¾Ã�Â±Ã�Â°Ã�Â²Ã�Â»Ã‘ï¿½Ã‘â€�Ã�Â¼Ã�Â¾ Ã�Â½Ã�Â¾Ã�Â²Ã‘â€“ case Ã�Â¿Ã�Â¾ Ã�Â½Ã‘Æ’Ã�Â¼Ã�ÂµÃ‘â‚¬Ã�Â°Ã‘â€ Ã‘â€“Ã‘â€” 1,2,3, ....
 		
-		while (true) {
+	while (true) {
+
+		switch (taskNum) {
 			
-			switch (taskNum) {
-			
-			case 1:
-				
+			case 1:{
 				Task.main(args);
-				
-			case 2:
-				
-				Task2.main(args);
-							
-			case 3:
-				
-				Task3.main(args);
-				
-			case 4:
-				
-				Task4.main(args);
-				
-			case 8:
-				
-				Task8.main(args);
-				
+				break;
 			}
+			case 2: {
+				Task2.main(args);
+				break;
+			}
+			case 3: {
+				Task3.main(args);
+				break;
+			}
+			case 4: {
+				Task4.main(args);
+				break;
+			}
+			case 5: {
+				//Task5.main(args);
+				break;
+			}
+			case 6: {
+				//Task6.main(args);
+				break;
+			}
+			case 7: {
+				Task7.main(args);
+				break;
+			}
+			case 8: {
+				Task8.main(args);
+				break;
+			}
+			case 9:
+				//Task9.main(args);
+				break;
+			case 10:
+				//Task10.main(args);
+				break;
+			case 11:
+				//Task11.main(args);
+				break;
+			case 12:
+				//Task12.main(args);
+				break;
+			case 13:
+				//Task13.main(args);
+				break;
+			case 14:
+				//Task14.main(args);
+				break;
+			case 15:
+				//Task15.main(args);
+				break;
+			case 16:
+				//Task16.main(args);
+				break;
+			case 17:
+				//Task17.main(args);
+				break;
+			case 18:
+				//Task18.main(args);
+				break;
+			case 19:
+				//Task19.main(args);
+				break;
+			case 20:
+				//Task20.main(args);
+				break;
+			case 21:
+				//Task21.main(args);
+				break;
+			case 22:
+				//Task22.main(args);
+				break;
+			case 23:
+				//Task23.main(args);
+				break;
+			case 24:
+				//Task24.main(args);
+				break;
+			case 25:
+				//Task25.main(args);
+				break;
+			case 26:
+				//Task26.main(args);
+				break;
+			case 27:
+				//Task27.main(args);
+				break;
+			case 28:
+				//Task28.main(args);
+				break;
+			case 29:
+				//Task29.main(args);
+				break;
+			case 30:
+				//Task30.main(args);
+				break;
+			case 31:
+				//Task31.main(args);
+				break;
+			case 32:
+				//Task32.main(args);
+				break;
+
+		}
+
+	}
+
+	}
+
 			
 		}
 		
