1be75400591f0e967ea3b2f21448d8897cafd220
Shum616 <48323638+Shum616@users.noreply.github.com>
2021-08-20 14:04:27 +0300

    Task4 and Task9 have been modified

--- javaClub/team3/Task4.java
+++ javaClub/team3/Task4.java
@@ -1,3 +1,4 @@
+
 package javaClub.team3;
 
 import java.io.BufferedReader;
@@ -9,22 +10,28 @@ import java.io.InputStreamReader;
 //the wall, otherwise it should return False. For example is_divisible(4050, 27) should return
 //True, but is_divisible(4066, 27)should return False.
 
-public class Task4 {
+public class Task4 extends Task{
 
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input  = null;
+    @Override
+    void execute() {
+        System.out.println("Please enter the size of the wall in millimeters and the size of a pixel in millimeters: ");
         try{
-            System.out.println("Please enter the size of the wall in millimeters and the size of a pixel in millimeters: ");
-            input  = br.readLine();
-        } catch (IOException e) {
+            String[] numbers = GetData();
+            while(numbers[0]== null || numbers[1] == null){
+                System.out.println("Something is wrong. Enter numbers again: ");
+                numbers = GetData();
+            }
+            while(Integer.parseInt(numbers[0]) < Integer.parseInt(numbers[1])){
+                System.out.println("The size of the wall can`t be smaller then the size of a pixel. Enter numbers again: ");
+                numbers = GetData();
+            }
+            if(IsDivisible(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1])) == true){
+                System.out.println("Your pixel art is going to be perfect!");
+            } else {
+                System.out.println("Oh no! You can`t fit an exact number of pixels on the wall!");
+            }
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        String[] numbers = input.split(" ");
-        if(IsDivisible(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1])) == true){
-            System.out.println("Your pixel art is going to be perfect!");
-        } else {
-            System.out.println("Oh no! You can`t fit an exact number of pixels on the wall!");
         }
     }
 
@@ -32,4 +39,10 @@ public class Task4 {
     {
         return wallLength%pixelSize == 0;
     }
+    public String[] GetData(){
+        String input  = scanner.nextLine();
+        String[] numbers = input.split(" ");
+        return numbers;
+    }
+
 }
