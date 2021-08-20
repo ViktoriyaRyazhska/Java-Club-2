f0e2b53b4e96ba778e5c2900c405643da07f3c00
DimaSannytski <sbymix91@gmail.com>
2021-08-20 13:13:49 +0300

    Tests for tasks 2 and 7

--- javaClub/team3/Task8.java
+++ javaClub/team3/Task8.java
@@ -1,8 +1,26 @@
 package javaClub.team3;
-//century-from-year
-public class Task8 {
 
+import java.io.IOException;
+
+//https://www.codewars.com/kata/century-from-year
+public class Task8 extends Task{
+	private int year;
 	public static int century(int number) {
 	    return (int)Math.ceil((double)number/100);
 	  }
+
+	@Override
+	void execute() {
+		try {
+			System.out.println("Please enter year: ");
+			year = Integer.parseInt(bufferedReader.readLine());
+	
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NumberFormatException e) {
+			System.err.println("Not a number!");
+		}
+		System.out.println("Century: " + century(year));
+		
+	}
 }
---
+++ out/production/javaClub/README.md
@@ -0,0 +1,8 @@
+# Java-Club-2
+## Team 5
+Diakiv Marta  
+Maksym Kuzbyt  
+Votus Liubomyr  
+Kychma Nazar  
+Huzar Dmitry  
+
