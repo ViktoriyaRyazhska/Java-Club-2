38db849b1e52a34b61ddb8cd64f689cebcba5b68
Eugene Voronko <34031302+eugenevoronko@users.noreply.github.com>
2021-08-21 17:46:37 +0300

    Update Task9.java

--- src/main/java/com/javaclub/team4/basicprogram/Task9.java
+++ src/main/java/com/javaclub/team4/basicprogram/Task9.java
@@ -20,13 +20,13 @@ public class Task9 extends Task {
 	@Override
 	public void runTask() {
 		
-		System.out.println("Please enter a non-negative number to transform it into binary format:");
+		System.out.println("Please enter a number to transform it to negative one:");
 		try {
 			n = Integer.parseInt(br.readLine());
 		} catch (NumberFormatException | IOException e) {
-			System.out.println("It seems that the number you entered is not an integer. Try again.");
+			System.out.println("It seems that the number you entered is not valid. Try again.");
 		}
-		System.out.println(n + " in binary is ");
+		System.out.println(makeNegative(n) + " is the resul.");
 	}
 
 	public static int makeNegative(final int x) {
