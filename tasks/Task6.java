1a0246ef407f652946c511ba786df83b78751975
AlexDanihevych <danifromalex@gmail.com>
2021-08-21 18:07:07 +0300

    deleted wrong files

--- tasks/Task6.java
+++
@@ -1,9 +0,0 @@
-package java_core;
-
-public class Task6 {
-	  public static int toBinary(int value) {
-		  int n = Integer.valueOf(Integer.toBinaryString(value));
-		      return n;
-		        }
-		     
-}
