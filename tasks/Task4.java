844f9e7ed5507359e10c1ba1e90576bb981be7fa
AlexDanihevych <danifromalex@gmail.com>
2021-08-22 11:08:02 +0300

    corrected Task4

--- tasks/Task4.java
+++ tasks/Task4.java
@@ -2,9 +2,21 @@ package java_core;
 
 public class Task4 {
 	
+	public static boolean isDivisible (int a, int b) {
+		
+		int div = int a % int b;
+		if ( div == 0) {
+			System.out.println("true");
+		}else {
+			System.out.println("False");
+		}
+		
+		return ;
+	}
+	
 	public static void main(String[] args) {
 		
-		//aaddasda
+		
 		
 	}
 
