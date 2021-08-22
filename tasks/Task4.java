80f5875525223cd19b391fb7b01d1c883b832a16
AlexDanihevych <danifromalex@gmail.com>
2021-08-22 11:10:37 +0300

    corrected Task4

--- tasks/Task4.java
+++ tasks/Task4.java
@@ -1,22 +1,30 @@
 package java_core;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task4 {
 	
 	public static boolean isDivisible (int a, int b) {
 		
-		int div = int a % int b;
+		int div = a %  b;
 		if ( div == 0) {
-			System.out.println("true");
+			return true;
 		}else {
-			System.out.println("False");
+			return false;
 		}
-		
-		return ;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the value of pixel");
+		int pix = Integer.parseInt(br.readLine());
+		System.out.println("Enter the value of milimeters");
+		int mm = Integer.parseInt(br.readLine());
 		
+		isDivisible(pix, mm);
 		
 	}
 
