65cd67fd88d25844208023e5c37d93453901a694
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-08-23 14:44:46 +0300

    editApplMain

--- src/tasks/ApplMain.java
+++ src/tasks/ApplMain.java
@@ -1,10 +1,41 @@
 package tasks;
 
+import java.util.Scanner;
+
 public class ApplMain {
 	public static void main(String[] args) {
-		
+
 		System.out.print("Hello! Input Task's number: ");
+		int numTask = 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			try {
+				numTask = Integer.parseInt(sc.nextLine());
+
+				break;
+			} catch (Exception e) {
+
+				System.out.print("Input integer number betwen 1 and 72: ");
+			}
+
+		} while (true);
+
+		switch (numTask) {
+		case 1: {
+			break;
+			
+		}case 2: {
+			break;
+			
+		}
 		
+		
+		}
+
+		sc.close();
+
 	}
 
 }
