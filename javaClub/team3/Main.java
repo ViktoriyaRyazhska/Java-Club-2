793daaa8d5cf2ab71c3c451845fc7c2c96cb3e36
Pavlo Skolozdra <pavlo.skolozdra@gmail.com>
2021-08-23 22:45:47 +0300

    Task 35, 40, 45, 50, 55, 60, 65, 70 added

--- javaClub/team3/Main.java
+++ javaClub/team3/Main.java
@@ -12,6 +12,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Map<Integer, Task> taskMap = new LinkedHashMap<>();
         taskMap.put(2, new Task2());
+        taskMap.put(3, new Task3());
         taskMap.put(5, new Task5());
         taskMap.put(7, new Task7());
         taskMap.put(10, new Task10());
@@ -21,7 +22,15 @@ public class Main {
         taskMap.put(23, new Task23());
         taskMap.put(28, new Task28());
         taskMap.put(33, new Task33());
-        taskMap.put(3, new Task3());
+        taskMap.put(35, new Task35());
+        taskMap.put(40, new Task40());
+        taskMap.put(45, new Task45());
+        taskMap.put(50, new Task50());
+        taskMap.put(55, new Task55());
+        taskMap.put(60, new Task60());
+        taskMap.put(65, new Task65());
+        taskMap.put(70, new Task70());
+
         taskMap.put(8, new Task8());
         while (true) {
             System.out.println("Please enter task number, 0 for exit: ");
