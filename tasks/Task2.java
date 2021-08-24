fb2f51357e46010b3aa443117f7c0f57c217986e
IriOtto <iriottosan@gmail.com>
2021-08-24 19:54:49 +0300

    Task 2 final

--- tasks/Task2.java
+++ tasks/Task2.java
@@ -2,25 +2,25 @@ package java_core;
 
 public class Task2 {
 	public static void main(String[] args) {
-		try {
+		
 		Scanner in = new Scanner(System.in);
-	    System.out.print("Введіть число 1: ");
+	    System.out.print("Firs number: ");
 	    int num1 = in.nextInt();
-	    System.out.print("Введіть число 2: ");
+	    System.out.print("Second number: ");
 	    int num2 = in.nextInt();
-	    System.out.print("Ваша відповідь: " + multiply(num1, num2));
-		}catch(ArithmeticException e) {
-        	System.out.println("Не коректне введення");
-        }
+	    System.out.print("Your answer: " + multiply(num1, num2));
+		
 		 }
 	
 	
 	
     public static int multiply(int num1, int num2) {
-           
+    	try {  
     	int num3 = (int) num1 * num2;
     	return num3;
-    	
+    	}catch(NumberFormatException e) {
+        	System.out.println("You must enter number");
+        }
     	
     } 
 }
