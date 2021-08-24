fc812ceb0bd9d1af2555fc28c05db7b5f4ec5016
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-24 12:37:25 +0300

    Task 17 added

---
+++ javaClub/team3/Task17.java
@@ -0,0 +1,68 @@
+package javaClub.team3;
+
+public class Task17 extends Task{
+    @Override
+    void execute() {
+        System.out.println("Print width, length, height values");
+        int []values = new int[3];
+        for (int i = 0; i < 3; i++) {
+            values[i] = scanner.nextInt();
+        }
+        Block block = new Block(values);
+        System.out.println("Volume: " + block.getVolume() + "\nSurface area: " + block.getSurfaceArea());
+    }
+}
+
+class Block{
+    private int width;
+    private int length;
+    private int height;
+    private int volume;
+    private int surfaceArea;
+
+    Block(int[] values) {
+        setWidth(values[0]);
+        setLength(values[1]);
+        setHeight(values[2]);
+        calc();
+    }
+
+    public void calc() {
+        int width = getWidth();
+        int length = getLength();
+        int height = getHeight();
+        setVolume(width*length*height);
+        setSurfaceArea((width*length+length*height+height*width)*2);
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public void setSurfaceArea(int surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public int getLength() {
+        return length;
+    }
+    public int getSurfaceArea() {
+        return surfaceArea;
+    }
+    public int getVolume() {
+        return volume;
+    }
+    public int getWidth() {
+        return width;
+    }
+}
