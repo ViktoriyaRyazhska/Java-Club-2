ca4c09d0fd248c4d7f45919923f6b62c5865bed0
KuzbytMaksym <max.dogmarion.lviv@gmail.com>
2021-08-24 13:04:25 +0300

    "" ""

--- test/basic/TestDoubleInteger.java
+++ test/basic/TestDoubleInteger.java
@@ -4,10 +4,16 @@ import com.softserve.academy.basic.DoubleInteger;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class TestDoubleInteger {
     @Test
     public final void test_all() {
         Assert.assertEquals("Nope!", 4, DoubleInteger.doubleInteger(2));
         Assert.assertEquals("Nope!", 6,  DoubleInteger.doubleInteger(3));
     }
+
 }
