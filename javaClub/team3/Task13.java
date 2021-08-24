fdc046ecba3a5608a01ec61634c7bed09164d190
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-24 17:42:34 +0300

    Task 13 tested

--- javaClub/team3/Task13.java
+++ javaClub/team3/Task13.java
@@ -7,15 +7,15 @@ public class Task13 extends Task{
 
 
 	  private static int ONE_HUNDRED = 100;
-	  
-	  public static final Task13 INST = new Task13();
-	    
-	  private final int value; 
 
-	  private Task13() {
+	  public static final Task13 INST = new Task13();
+
+	  private final int value;
+
+	  public Task13() {
 	    value = ONE_HUNDRED;
 	  }
-	  
+
 	  public int plus100(int n) {
 	    return value + n;
 	  }
@@ -35,6 +35,4 @@ public class Task13 extends Task{
 		
 	}
 
-
-		
 }
--- javaClub/team3/Tests/Task13Test.java
+++ javaClub/team3/Tests/Task13Test.java
@@ -1,4 +1,26 @@
 package javaClub.team3.Tests;
 
+import javaClub.team3.Task13;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Task13Test {
+    @Test
+    public void testing() {
+        assertEquals(123, Task13.INST.plus100(23));
+        assertEquals(100, Task13.INST.plus100(0));
+        assertEquals(77, Task13.INST.plus100(-23));
+    }
+
+    @Test
+    public void randomTesting() {
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int a = random.nextInt(10000)-5000;
+            assertEquals(a+100, Task13.INST.plus100(a));
+        }
+    }
 }
