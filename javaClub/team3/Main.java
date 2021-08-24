2b940ca91f3d4ee9326f23af3ca2e66b3fd9049d
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-24 12:15:40 +0300

    Update Main.java
    Added spaces

--- javaClub/team3/Main.java
+++ javaClub/team3/Main.java
@@ -11,6 +11,7 @@ public class Main {
         int taskNumber = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Map<Integer, Task> taskMap = new LinkedHashMap<>();
+        
 //      taskMap.put(1, new Task1());
         taskMap.put(2, new Task2());
         taskMap.put(3, new Task3());
@@ -21,6 +22,7 @@ public class Main {
         taskMap.put(8, new Task8());
 //      taskMap.put(9, new Task9());
         taskMap.put(10, new Task10());
+        
         taskMap.put(11, new Task11());
 //      taskMap.put(12, new Task12());
 //      taskMap.put(13, new Task13());
@@ -31,6 +33,7 @@ public class Main {
         taskMap.put(18, new Task18());
         taskMap.put(19, new Task19());
         taskMap.put(20, new Task20());
+        
 //      taskMap.put(21, new Task21());
 //      taskMap.put(22, new Task21());
         taskMap.put(23, new Task23());
@@ -41,6 +44,7 @@ public class Main {
         taskMap.put(28, new Task28());
         taskMap.put(29, new Task29());
 //      taskMap.put(30, new Task30());
+        
 //      taskMap.put(31, new Task31());
 //      taskMap.put(32, new Task32());
         taskMap.put(33, new Task33());
@@ -51,6 +55,7 @@ public class Main {
 //      taskMap.put(38, new Task34());
 //      taskMap.put(39, new Task34());
         taskMap.put(40, new Task40());
+        
 //      taskMap.put(41, new Task34());
 //      taskMap.put(42, new Task34());
 //      taskMap.put(43, new Task34());
@@ -61,6 +66,7 @@ public class Main {
 //      taskMap.put(48, new Task34());
 //      taskMap.put(49, new Task34()); 
         taskMap.put(50, new Task50());
+        
 //      taskMap.put(51, new Task34());
 //      taskMap.put(52, new Task34());
 //      taskMap.put(53, new Task34());
@@ -71,6 +77,7 @@ public class Main {
 //      taskMap.put(58, new Task34());
 //      taskMap.put(59, new Task34());        
         taskMap.put(60, new Task60());
+        
 //      taskMap.put(61, new Task34());
 //      taskMap.put(62, new Task34());
 //      taskMap.put(63, new Task34());
@@ -81,6 +88,7 @@ public class Main {
 //      taskMap.put(68, new Task34());
 //      taskMap.put(69, new Task34());         
         taskMap.put(70, new Task70());
+        
 //      taskMap.put(71, new Task34());
 //      taskMap.put(72, new Task34());      
         
