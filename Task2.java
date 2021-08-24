ca96d2f3d1bf95064690182531336e51207b94d8
IriOtto <iriottosan@gmail.com>
2021-08-24 14:49:00 +0300

    Task2v2.0

--- Task2.java
+++ Task2.java
@@ -1,8 +1,27 @@
 package java_core;
 
 public class Task2 {
+	public static void main(String[] args) {
+		try {
+		Scanner in = new Scanner(System.in);
+	    System.out.print("Введіть число 1: ");
+	    int num1 = in.nextInt();
+	    System.out.print("Введіть число 2: ");
+	    int num2 = in.nextInt();
+	    System.out.print("Ваша відповідь: " + multiply(num1, num2));
+		}catch(ArithmeticException e) {
+        	System.out.println("Не коректне введення");
+        }
+		 }
+	
+	
+	
+	
     public static int multiply(int num1, int num2) {
            
-      return num1 * num2;
+        	int num3 = (int) num1 * num2;
+        	return num3;
+        
+        
     } 
 }
