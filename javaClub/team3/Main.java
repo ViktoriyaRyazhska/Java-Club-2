758b341631d13adfe78f2ca6d1c80a078cd1e566
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-24 12:51:33 +0300

    Task 22

--- javaClub/team3/Main.java
+++ javaClub/team3/Main.java
@@ -34,7 +34,7 @@ public class Main {
         taskMap.put(20, new Task20());
 
 //        taskMap.put(21, new Task21());
-//        taskMap.put(22, new Task21());
+        taskMap.put(22, new Task22());
         taskMap.put(23, new Task23());
         taskMap.put(24, new Task24());
         taskMap.put(25, new Task25());
