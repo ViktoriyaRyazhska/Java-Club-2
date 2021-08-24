a9a330bf73dc1e40b48854966e929908b328441d
a1 <arturkondra31@gmail.com>
2021-08-24 13:11:35 +0300

    Add task1

---
+++ src/tasks/Task1.java
@@ -0,0 +1,22 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your integer number:");
+        int i;
+        do {
+            try {
+                String s = scan.nextLine();
+                i = Integer.parseInt(s);
+                break;
+            }
+            catch (Exception wrongInputDataType) {
+                System.out.println("Wront input data type...");
+            }
+        } while (true);
+        System.out.println(2 * i);
+    }
+}
