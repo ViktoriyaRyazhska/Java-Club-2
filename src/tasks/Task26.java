172e0b8e0f661b969959e8b7b4d9bd12198deb78
taras.krasitskyi <taras.krasitsky@gmail.com>
2021-08-25 18:22:38 +0300

    task26

--- src/tasks/Task26.java
+++ src/tasks/Task26.java
@@ -28,24 +28,50 @@ public class Task26 {
 						+ "\t\t* true, or a \"No\" string for false..\n");
 		Scanner sc = new Scanner(System.in);
 		boolean vBoolean = true;
+		boolean check = true;
 		String inputString = "";
-		System.out.print("\tInput boolean (true/false): ");
 
 		do {
-			try {
-				inputString = sc.nextLine();
-				if(inputString.equals("true") || inputString.equals("false")) {
-					vBoolean = Boolean.parseBoolean(inputString);
-				}else {
-					throw new RuntimeException ("Error. Bad boolean!  ");
+			System.out.print("\tInput boolean (true/false): ");
+			do {
+				try {
+					inputString = sc.nextLine();
+					if (inputString.equals("true") || inputString.equals("false")) {
+						vBoolean = Boolean.parseBoolean(inputString);
+					} else {
+						throw new RuntimeException("Error. Bad boolean!  ");
+					}
+
+					break;
+				} catch (Exception badBoolean) {
+					System.out.print("\t" + badBoolean.getMessage() + "Input \'true\' or \'false\': ");
 				}
-				
-				break;
-			} catch (Exception badBoolean) {
-				System.out.print("\t"+badBoolean.getMessage()+"Input \'true\' or \'false\': " );
+
+			} while (true);
+			System.out.println("Result: " + Task26.boolToWord(vBoolean));
+			System.out.print("\tDo you want execute again? (Y/N): ");
+			do {
+				try {
+					inputString = sc.nextLine();
+					if (inputString.equals("Y") || inputString.equals("N")) {
+						check = true;
+					} else {
+						throw new RuntimeException("Error. Bad answer!  ");
+					}
+
+					break;
+				} catch (Exception badAnswer) {
+					System.out.print("\t" + badAnswer.getMessage() + "Input \'Y\' or \'N\': ");
+				}
+
+			} while (true);
+
+			if (inputString.equals("N")) {
+				check = false;
+				System.out.print("\tApplication terminated!");
 			}
-		} while (true);
-		System.out.print("Result: " + Task26.boolToWord(vBoolean));
+
+		} while (check);
 
 		sc.close();
 
