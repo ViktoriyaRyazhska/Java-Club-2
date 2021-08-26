d05b032628a18979195998c921c68176e7d4a4fe
AlexDanihevych <danifromalex@gmail.com>
2021-08-26 19:29:42 +0300

    added test 11 and 13

---
+++ tests/Task13Test.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java_core.Task13;
+
+class Task13Test {
+
+	@Test
+	void test() {
+		assertEquals(110, Task13.AddHundred(10));
+	}
+
+}
