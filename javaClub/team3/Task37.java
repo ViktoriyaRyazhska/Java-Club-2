281597e8479ccb103a9ebcb1d45e838022d69ee5
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 12:25:32 +0300

    Task37 validation fix Test comment

--- javaClub/team3/Task37.java
+++ javaClub/team3/Task37.java
@@ -1,31 +1,42 @@
 package javaClub.team3;
-
+//https://www.codewars.com/kata/reversed-sequence/
 /* @Test
- * call
+ * call Sequence.reverse
  *
  * Exceptions:
+ * InputMismatchException when n <= 0
  */
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//https://www.codewars.com/kata/reversed-sequence/
 public class Task37 extends Task{
     @Override
     void execute() {
         Scanner scanner = new Scanner(System.in);
+        int n;
         System.out.println("Print number to get array");
-        Sequence sequence = new Sequence();
-        int[] res = sequence.reverse(scanner.nextInt());
+        try {
+            n = scanner.nextInt();
+            if (n <= 0) throw new InputMismatchException();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
+        }
+        int[] res = Sequence.reverse(scanner.nextInt());
         System.out.println(Arrays.toString(res));
+    }
+
+    public static class Sequence{
+        public static int[] reverse(int n){
+            if (n <= 0) throw new InputMismatchException();
+
+            int[] res = new int[n];
+            for (int i = n; i > 0; i--) {
+                res[n-i] = i;
+            }
+            return res;
+        }
     }
 }
 
-class Sequence{
-    public int[] reverse(int n){
-        int[] res = new int[n];
-        for (int i = n; i > 0; i--) {
-            res[n-i] = i;
-        }
-        return res;
-    }
-}
