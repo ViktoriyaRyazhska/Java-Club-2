3505b725bfae3b76b1bd1cbcda4541323b1b1be6
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 18:19:55 +0300

    Task3Test.java added

--- javaClub/team3/Tests/Task3Test.java
+++ javaClub/team3/Tests/Task3Test.java
@@ -2,8 +2,11 @@ package javaClub.team3.Tests;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.InputMismatchException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static javaClub.team3.Task3.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Task3Test {
     @Test
@@ -12,5 +15,17 @@ public class Task3Test {
         assertEquals(8, getVolumeOfCuboid(2, 2, 2));
         assertEquals(6, getVolumeOfCuboid(1, 2, 3));
         assertEquals(1000, getVolumeOfCuboid(10, 10, 10));
+    }
+
+    @Test
+    void negativeTest_Zero() {
+        assertThrows(InputMismatchException.class,
+                () -> System.err.println(getVolumeOfCuboid(0, 0, 0) + " is result, but no result expect!"));
+    }
+
+    @Test
+    void negativeTest_minus() {
+        assertThrows(InputMismatchException.class,
+                () -> System.err.println(getVolumeOfCuboid(-5, 5, 1) + " is result, but no result expect!"));
     }
 }
