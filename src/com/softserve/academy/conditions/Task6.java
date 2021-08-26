6f45481c1854aa73c36bf61f94f416bf71d2c2a9
Nazar Kychma <Nazar Kychma@DESKTOP-BVOPMP4>
2021-08-26 17:03:09 +0300

    One more fix

--- src/com/softserve/academy/conditions/Task6.java
+++ src/com/softserve/academy/conditions/Task6.java
@@ -3,13 +3,11 @@ package com.softserve.academy.conditions;
 public class Task6 {
     public static String updateLight(String current) {
         if (current == null) throw new NullPointerException();
-        if (!current.equals("green") || !current.equals("yellow") || !current.equals("red")) {
-            throw new IllegalArgumentException();
-        }
         switch (current) {
             case "green" -> { return "yellow"; }
             case "yellow" -> { return "red"; }
-            default -> { return "green"; }
+            case "red" -> { return "green"; }
+            default -> { throw new IllegalArgumentException(); }
         }
     }
 }
