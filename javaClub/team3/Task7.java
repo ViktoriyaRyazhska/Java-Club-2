f576d49eb1415fb909e217a4f38406ee80624472
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 11:05:46 +0300

    Task7 validation

--- javaClub/team3/Task7.java
+++ javaClub/team3/Task7.java
@@ -6,6 +6,7 @@
 
 package javaClub.team3;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task7 extends Task{
@@ -14,9 +15,38 @@ public class Task7 extends Task{
         Scanner scanner = new Scanner(System.in);
         int h, m, s;
         System.out.println("Input hours");
+        try {
+            h = scanner.nextInt();
+            if (h < 0 || h > 23) throw new InputMismatchException();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
+        }
+        System.out.println("Input minutes");
+        try {
+            m = scanner.nextInt();
+            if (m < 0 || m > 59) throw new InputMismatchException();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
+        }
+        System.out.println("Input hours");
+        try {
+            s = scanner.nextInt();
+            if (s < 0 || s > 59) throw new InputMismatchException();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
+        }
+        System.out.println("Result: " + Past.past(h, m, s));
     }
 
-    public static long past(int h, int m, int s) {
-        return ((((h* 60L)+m)*60)+s)*1000;
+    public static class Past {
+        public static long past(int h, int m, int s) {
+            if (h < 0 || h > 23) throw new InputMismatchException();
+            if (m < 0 || m > 59) throw new InputMismatchException();
+            if (s < 0 || s > 59) throw new InputMismatchException();
+            return ((((h* 60L)+m)*60)+s)*1000;
+        }
     }
 }
