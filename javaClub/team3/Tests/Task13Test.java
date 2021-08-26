bc5fe4284aceb719693261ce546fa0d920398c59
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 18:30:17 +0300

    Task13Test.java added

--- javaClub/team3/Tests/Task13Test.java
+++ javaClub/team3/Tests/Task13Test.java
@@ -3,9 +3,11 @@ package javaClub.team3.Tests;
 import javaClub.team3.Task13;
 import org.junit.jupiter.api.Test;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Task13Test {
     @Test
@@ -22,5 +24,11 @@ public class Task13Test {
             int a = random.nextInt(10000)-5000;
             assertEquals(a+100, Task13.INST.plus100(a));
         }
+    }
+
+    @Test
+    void negativeTest() {
+        assertThrows(InputMismatchException.class,
+                () -> System.err.println(Task13.INST.plus100(Integer.MAX_VALUE-50) + " is result, but no result expect!"));
     }
 }
