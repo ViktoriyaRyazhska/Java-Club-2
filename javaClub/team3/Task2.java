1e791ac4ae59692c51547578b90c92afef0e4863
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 10:46:53 +0300

    Task2 user validation Task2 test validation Task2Test

--- javaClub/team3/Task2.java
+++ javaClub/team3/Task2.java
@@ -6,20 +6,44 @@
 
 package javaClub.team3;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Task2 extends Task{
     @Override
     void execute() {
-        System.out.println("Input 2 numbers");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input first (a) number to multiply");
+        int a;
         try {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            System.out.println("Multiply: " + multiply(a, b));
-        } catch (Exception e) {
-            e.printStackTrace();
+            a = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input a");
+            return;
         }
+        System.out.println("Input second (b) number to multiply");
+        int b;
+        try {
+            b = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input b");
+            return;
+        }
+        try {
+            if ((long)a*b > Integer.MAX_VALUE) throw new InputMismatchException();
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Result: " + Multiply.multiply(a, b));
     }
 
-    public static int multiply(int num1, int num2) {
-        return num1*num2;
+    public static class Multiply {
+        public static int multiply(int num1, int num2) {
+            if ((long)num1*num2 > Integer.MAX_VALUE) throw new InputMismatchException();
+            return num1*num2;
+        }
     }
 }
