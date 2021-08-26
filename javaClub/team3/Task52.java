df72793a8be84cf16309e5ab4f510d57395c0553
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 14:01:39 +0300

    Task52 validation fix Test comment

--- javaClub/team3/Task52.java
+++ javaClub/team3/Task52.java
@@ -1,21 +1,36 @@
 package javaClub.team3;
+//https://www.codewars.com/kata/reversed-strings
 
+/* @Test
+ * call Kata52.solution
+ *
+ * Exceptions:
+ * InputMismatchException when empty string
+ */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//
 public class Task52 extends Task{
     @Override
     void execute() {
         Scanner scanner = new Scanner(System.in);
+        String value;
         System.out.println("Print string to reverse");
-        System.out.println(new Kata52().solution(scanner.nextLine()));
+        try {
+            value = scanner.nextLine();
+            if (value.length() == 0) throw new InputMismatchException();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
+        }
+        System.out.println(Kata52.solution(value));
+    }
+
+    public static class Kata52 {
+        public static String solution(String str) {
+            if (str.length() == 0) throw new InputMismatchException();
+            return new StringBuilder().append(str).reverse().toString();
+        }
     }
 }
 
-class Kata52 {
-
-    public String solution(String str) {
-        return new StringBuilder().append(str).reverse().toString();
-    }
-
-}
