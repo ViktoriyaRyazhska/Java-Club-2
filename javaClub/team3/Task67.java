1a7c0fefc4215a6ebe927f1b2673e0ace8878150
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 14:13:37 +0300

    Task67 validation fix Test comment

--- javaClub/team3/Task67.java
+++ javaClub/team3/Task67.java
@@ -1,32 +1,49 @@
 package javaClub.team3;
+// https://www.codewars.com/kata/alternating-case-<-equals->-alternating-case/
+// copy/paste url
 
+/* @Test
+ * call StringUtils.toAlternativeString
+ *
+ * Exceptions:
+ * no exceptions
+ */
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//https://www.codewars.com/kata/alternating-case-<-equals->-alternating-case/
 public class Task67 extends Task{
     @Override
     void execute() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Print string to Up/LowerCase");
-        System.out.println(new StringUtils().toAlternativeString(scanner.nextLine()));
-    }
-}
-
-class StringUtils {
-    public String toAlternativeString(String string) {
-        String res = "";
-
-        for (char a : string.toCharArray()) {
-            if (a >= 65 && a <= 90) {
-                a+=32;
-            } else {
-                if (a >= 97 && a <= 122) {
-                    a-=32;
-                }
-            }
-            res+=a;
+        String str;
+        System.out.println("Print string to Up/LowerCase (sAMplE)");
+        try {
+            str = scanner.nextLine();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
         }
+        System.out.println(StringUtils.toAlternativeString(str));
+    }
 
-        return res;
+    public static class StringUtils {
+        public static String toAlternativeString(String string) {
+            StringBuilder res = new StringBuilder();
+
+            for (char a : string.toCharArray()) {
+                if (a >= 65 && a <= 90) {
+                    a+=32;
+                } else {
+                    if (a >= 97 && a <= 122) {
+                        a-=32;
+                    }
+                }
+                res.append(a);
+            }
+
+            return res.toString();
+        }
     }
 }
+
