3a99fa4593f19da0ba8d4ef77204ca6fd27e5c4a
solomchaak <iviksb3@gmail.com>
2021-08-26 19:14:48 +0300

    Added task 9

--- tasks/Task9.java
+++ tasks/Task9.java
@@ -3,12 +3,49 @@ package java_core;
 import java.util.Scanner;
 
 public class Task9 {
+    public static String ReturnNegative(String  word){
+        if ((word.charAt(0) == '-') || (word.equals("0")) ){
+            return word;
+        } else
+            return "-" + word;
+    }
+
+    public static boolean isNumeric(String x) {
+        return isInt(x) || isDouble(x);
+    }
+    
+    public static boolean isInt(String x) throws NumberFormatException {
+        try {
+            Integer.parseInt(x);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isDouble(String x) throws NumberFormatException {
+        try {
+            Double.parseDouble(x);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.println("Enter your integer number:");
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
-        /*if (s[0] == "-"){
 
-        }*/
-        System.out.println("-" + s);
+        if (isNumeric(s)){
+            if(isInt(s)){
+                System.out.println(ReturnNegative(s));
+            } else if (isDouble(s)){
+                System.out.println(ReturnNegative(s));
+            } else return;
+        } else {
+            System.out.println("Wrong input data type...");
+            return;
+        }
     }
 }
