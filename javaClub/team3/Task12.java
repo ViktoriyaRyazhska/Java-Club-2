902d100a53431cf4115724845fc202be340fb9c4
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 11:07:33 +0300

    Task12 fixed

--- javaClub/team3/Task12.java
+++ javaClub/team3/Task12.java
@@ -7,38 +7,39 @@ public class Task12 extends Task{
     void execute() {
         System.out.println("Nothing to output");
     }
+
+    public static class EncapsulationDemo{
+        private int number;
+        private String stringValue;
+        private Object anObject;
+
+        EncapsulationDemo(){}
+        EncapsulationDemo(int number, String string, Object anObject) {
+            setNumber(number);
+            setStringValue(string);
+            setAnObject(anObject);
+        }
+
+        public int getNumber() {
+            return number;
+        }
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public String getStringValue() {
+            return stringValue;
+        }
+        public void setStringValue(String stringValue) {
+            this.stringValue = stringValue;
+        }
+
+        public Object getAnObject() {
+            return anObject;
+        }
+        public void setAnObject(Object anObject) {
+            this.anObject = anObject;
+        }
+    }
 }
 
-class EncapsulationDemo{
-    private int number;
-    private String stringValue;
-    private Object anObject;
-
-    EncapsulationDemo(){}
-    EncapsulationDemo(int number, String string, Object anObject) {
-        setNumber(number);
-        setStringValue(string);
-        setAnObject(anObject);
-    }
-
-    public int getNumber() {
-        return number;
-    }
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getStringValue() {
-        return stringValue;
-    }
-    public void setStringValue(String stringValue) {
-        this.stringValue = stringValue;
-    }
-
-    public Object getAnObject() {
-        return anObject;
-    }
-    public void setAnObject(Object anObject) {
-        this.anObject = anObject;
-    }
-}
