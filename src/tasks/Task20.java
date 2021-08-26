0a92a88b21f08cdc35d17ae79a0db65f974be38e
Yuriy <kuzo.yuriy@gmail.com>
2021-08-26 16:52:15 +0300

    add Task20, uncommented ApplMain.java for this task

---
+++ src/tasks/Task20.java
@@ -0,0 +1,42 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Task20 {
+    public static void main(String[] args) {
+        System.out.println("\tTask 20.\n\t\tBoolean to string\n");
+        System.out.println("Implement a function which convert the given boolean value into its string representation.");
+        System.out.print("Enter a boolean value (true or false): ");
+
+        Scanner sc = new Scanner(System.in);
+        boolean bool;
+        String str;
+                do {
+            try {
+                str = sc.nextLine();
+                if (str.equals("false")) {
+                    bool = false;
+                    System.out.println(convert(bool));
+                    break;
+                } else if (str.equals("true")) {
+                    bool = true;
+                    System.out.println(convert(bool));
+                    break;
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                System.out.println("Entered non-boolean variable. Please enter true or false");
+                System.out.print("Enter a boolean value: ");
+            }
+        } while (true);
+
+    }
+    public static String convert(boolean b){
+        if(b == true) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+}
