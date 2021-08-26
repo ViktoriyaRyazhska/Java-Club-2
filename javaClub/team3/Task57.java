eba10df770839c72f18268adfdf064ab7e7b98ff
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 14:05:15 +0300

    Task57 validation fix Test comment

--- javaClub/team3/Task57.java
+++ javaClub/team3/Task57.java
@@ -1,19 +1,33 @@
 package javaClub.team3;
+//https://www.codewars.com/kata/remove-string-spaces/
 
+/* @Test
+ * call Kata57.noSpace
+ *
+ * Exceptions:
+ * no exceptions
+ */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//https://www.codewars.com/kata/remove-string-spaces/
 public class Task57 extends Task{
     @Override
     void execute() {
         Scanner scanner = new Scanner(System.in);
+        String str;
         System.out.println("Print string to remove spaces");
-        System.out.println(new Kata57().noSpace(scanner.nextLine()));
+        try {
+            str = scanner.nextLine();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
+        }
+        System.out.println(Kata57.noSpace(str));
+    }
+    public static class Kata57 {
+        public static String noSpace(final String x) {
+            return x.replaceAll(" ", "");
+        }
     }
 }
 
-class Kata57 {
-    public String noSpace(final String x) {
-        return x.replaceAll(" ", "");
-    }
-}
