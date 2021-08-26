18873978f746e6167bd5a9286b8c8b99bec4aadf
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-26 11:55:33 +0300

    Task27 validation fix Test comment

--- javaClub/team3/Task27.java
+++ javaClub/team3/Task27.java
@@ -2,23 +2,55 @@ package javaClub.team3;
 
 //https://www.codewars.com/kata/will-there-be-enough-space/
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
+/* @Test
+ * call Bob.enough
+ *
+ * Exceptions
+ * InputMismatchException when cap <= 0
+ * InputMismatchException when any value < 0
+ */
 public class Task27 extends Task{
     @Override
     void execute() {
         Scanner scanner = new Scanner(System.in);
-        Bob bob = new Bob();
-        System.out.println("Print cap, on and wait passengers values");
-        int cap = scanner.nextInt();
-        int on = scanner.nextInt();
-        int wait = scanner.nextInt();
-        System.out.println(bob.enough(cap, on, wait));
+        int cap, on, wait;
+        System.out.println("Print cap bus value");
+        try {
+            cap = scanner.nextInt();
+            if (cap <= 0) throw new InputMismatchException();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
+        }
+        try {
+            on = scanner.nextInt();
+            if (on < 0) throw new InputMismatchException();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
+        }
+        try {
+            wait = scanner.nextInt();
+            if (wait < 0) throw new InputMismatchException();
+        } catch (InputMismatchException e) {
+            System.err.println("Incorrect input");
+            return;
+        }
+        System.out.println("Print how many passengers on bus");
+        System.out.println("Print how many passengers wait for bus");
+        System.out.println(Bob.enough(cap, on, wait));
+    }
+    public static class Bob {
+        public static int enough(int cap, int on, int wait){
+            if (cap <= 0) throw new InputMismatchException();
+            if (on < 0) throw new InputMismatchException();
+            if (wait < 0) throw new InputMismatchException();
+
+            return (Math.max((on + wait) - cap, 0));
+        }
     }
 }
 
-class Bob {
-    public static int enough(int cap, int on, int wait){
-        return (((on+wait)-cap >= 0) ? (on+wait)-cap : 0);
-    }
-}
+
