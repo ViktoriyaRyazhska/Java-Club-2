cc59cd9ace3f2f0fdbd19859f2ec324bf123e4ed
KuzbytMaksym <max.dogmarion.lviv@gmail.com>
2021-08-26 20:05:16 +0300

    refactored test for 56 and 57  task

--- test/string/Task55Test.java
+++ test/string/Task55Test.java
@@ -9,14 +9,14 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Task55Test {
-    String test = null;
+
     @Test
     public void testSomeDots() {
         assertTrue("Sorry, try again :-(", "one-two-three".equals(Task55.replaceDots("one.two.three")));
     }
     @Test
     public void notNullTest (){
-        assertThrows(NullPointerException.class,() ->  Task55.replaceDots(test));
+        assertThrows(NullPointerException.class,() ->  Task55.replaceDots(null));
     }
 }
 
--- test/string/Task56Test.java
+++ test/string/Task56Test.java
@@ -1,10 +1,12 @@
 package string;
 
+import com.softserve.academy.string.Task55;
 import com.softserve.academy.string.Task56;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Task56Test {
 
@@ -13,5 +15,9 @@ public class Task56Test {
         assertEquals( "Nope!", "Martin does not play banjo", Task56.areYouPlayingBanjo("Martin"));
         assertEquals( "Nope!", "Rikke plays banjo", Task56.areYouPlayingBanjo("Rikke"));
     }
-
+    @Test
+    public void notNullTest (){
+        assertThrows(NullPointerException.class,() ->  Task55.replaceDots(null));
+    }
 }
+
