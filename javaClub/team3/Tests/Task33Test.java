020d4b2bc2039e78ae9cbdec20cfd4f2419c5cf6
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-08-27 09:54:28 +0300

    Task33Test.java added

--- javaClub/team3/Tests/Task33Test.java
+++ javaClub/team3/Tests/Task33Test.java
@@ -1,4 +1,44 @@
 package javaClub.team3.Tests;
 
+import javaClub.team3.Task33;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class Task33Test {
+    @Test
+    void normalTest() {
+        outputBefore(1);
+        assertEquals(Arrays.toString(new int[]{1,15,15}), Arrays.toString(Task33.humanYearsCatYearsDogYears(1)));
+        outputAfter();
+        outputBefore(2);
+        assertEquals(Arrays.toString(new int[]{2,24,24}), Arrays.toString(Task33.humanYearsCatYearsDogYears(2)));
+        outputAfter();
+        outputBefore(10);
+        assertEquals(Arrays.toString(new int[]{10,56,64}), Arrays.toString(Task33.humanYearsCatYearsDogYears(10)));
+        outputAfter();
+    }
+
+    @Test
+    void negativeTest() {
+        outputBefore(0);
+        assertThrows(InputMismatchException.class,
+                () -> System.err.println(Arrays.toString(Task33.humanYearsCatYearsDogYears(0)) + " is result, but no result expect!"));
+        outputAfter();
+        outputBefore(-5);
+        assertThrows(InputMismatchException.class,
+                () -> System.err.println(Arrays.toString(Task33.humanYearsCatYearsDogYears(-5)) + " is result, but no result expect!"));
+        outputAfter();
+    }
+
+    void outputBefore(int par1) {
+        System.out.println("Input params: " + par1);
+    }
+    void outputAfter() {
+        System.out.println("OK!");
+    }
 }
