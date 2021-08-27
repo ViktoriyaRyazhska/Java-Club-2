54e3da261303d4566a1ed5839e4b76059f83f13f
CobraJava <arturkondra31@gmail.com>
2021-08-27 12:30:25 +0300

    Add task17 new

--- src/tasks/Task17.java
+++ src/tasks/Task17.java
@@ -4,15 +4,24 @@ import java.util.Scanner;
 
 /*
 https://www.codewars.com/kata/building-blocks/
-You are provided with a skeleton of the class 'Fraction', which accepts two arguments (numerator, denominator).
-EXAMPLE:
-Fraction fraction1 = new Fraction(4, 5);
-Your task is to make this class string representable, and addable while keeping the result in the minimum representation possible.
-EXAMPLE:
-System.out.println(fraction1.add(new Fraction(1, 8)));
-// Outputs: 37/40
-NB: DON'T use the built_in class 'fractions.Fraction'
+Write a class Block that creates a block (Duh..)
+##Requirements
+The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+Define these methods:
+`getWidth()` return the width of the `Block`
+`getLength()` return the length of the `Block`
+`getHeight()` return the height of the `Block`
+`getVolume()` return the volume of the `Block`
+`getSurfaceArea()` return the surface area of the `Block`
+##Examples
+    Block b = new Block(new int[]{2,4,6}) -> creates a `Block` object with a width of `2` a length of `4` and a height of `6`
+    b.getWidth() // -> 2
+    b.getLength() // -> 4
+    b.getHeight() // -> 6
+    b.getVolume() // -> 48
+    b.getSurfaceArea() // -> 88
  */
+
 public class Task17{
 
     public static void main(String[] args) {
