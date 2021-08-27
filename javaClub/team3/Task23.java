5ce43acdc42d15f713bcc43e09ed7fae1d9419bf
Pavlo Skolozdra <pavlo.skolozdra@gmail.com>
2021-08-27 11:10:32 +0300

    Tests 11,12,16,26,31,32, 36, 41, 51, 52, 56, 66, 71, 72 added

--- javaClub/team3/Task23.java
+++ javaClub/team3/Task23.java
@@ -6,66 +6,62 @@ import java.util.InputMismatchException;
 //https://www.codewars.com/kata/rock-paper-scissors/
 public class Task23 extends Task {
 
-	private String p1, p2;
-	private static String result;
-	public static String rps(String p1, String p2) {
+    private String p1, p2;
+    private static String result;
 
-		if (p1 == null || p2 == null) throw new InputMismatchException();
-		
-		if (!(isValidInput(p1) && isValidInput(p2))) throw new InputMismatchException();
-		
-		if (p1.equals(p2))
-			return "Draw!";
+    public static String rps(String p1, String p2) {
 
-		if (p1.equals("scissors")) {
-			if (p2.equals("paper"))
-				setResult(1);
-			else
-				setResult(2);
+        if (p1 == null || p2 == null) throw new InputMismatchException();
 
-		} else if (p1.equals("paper")) {
-			if (p2.equals("rock"))
-				setResult(1);
-			else
-				setResult(2);
-		} else {
-			if (p2.equals("scissors"))
-				setResult(1);
-			else
-				setResult(2);
-		}
-		return result;
-	}
+        if (!(isValidInput(p1) && isValidInput(p2))) throw new InputMismatchException();
 
-public static void setResult(int playerNumber) {
-		result = "Player " + playerNumber + " won!";
+        if (p1.equals(p2))
+            return "Draw!";
 
-	public static boolean isValidInput(String p) {
+        if (p1.equals("scissors")) {
+            if (p2.equals("paper"))
+                setResult(1);
+            else
+                setResult(2);
 
-		
-		return p.equals("scissors") || p.equals("paper") || p.equals("rock");
-		
-	}
-	public static void setResoult(int playerNumber) {
-		resoult = "Player " + playerNumber + " won!";
+        } else if (p1.equals("paper")) {
+            if (p2.equals("rock"))
+                setResult(1);
+            else
+                setResult(2);
+        } else {
+            if (p2.equals("scissors"))
+                setResult(1);
+            else
+                setResult(2);
+        }
+        return result;
+    }
 
-	}
+    public static void setResult(int playerNumber) {
+        result = "Player " + playerNumber + " won!";
+    }
 
-	@Override
-	void execute() {
-		
-		try {
-			
-			System.out.println("Please enter player 1: ");
-			String p1 = bufferedReader.readLine();
-			System.out.println("Please enter player 2: ");
-			String p2 = bufferedReader.readLine();
-			System.out.println(rps(p1, p2));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+    public static boolean isValidInput (String p){
+        return p.equals("scissors") || p.equals("paper") || p.equals("rock");
+    }
 
-	}
+    public static void setResoult ( int playerNumber){
+        result = "Player " + playerNumber + " won!";
+    }
 
+    @Override
+    void execute() {
+
+        try {
+
+            System.out.println("Please enter player 1: ");
+            String p1 = bufferedReader.readLine();
+            System.out.println("Please enter player 2: ");
+            String p2 = bufferedReader.readLine();
+            System.out.println(rps(p1, p2));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- javaClub/team3/Tests/Task41Test.java
+++ javaClub/team3/Tests/Task41Test.java
@@ -1,4 +1,39 @@
 package javaClub.team3.Tests;
 
+import javaClub.team3.Task41;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class Task41Test {
+    @Test
+    public void exampleTest1() {
+        assertArrayEquals(new String[]{ "head", "body", "tail" },
+                Task41.fixTheMeerkat(new String[]{ "tail", "body", "head" }));
+    }
+
+    @Test
+    public void exampleTest2() {
+        assertArrayEquals(new String[]{ "heads", "body", "tails" },
+                Task41.fixTheMeerkat(new String[]{ "tails", "body", "heads" }));
+    }
+
+
+    @Test
+    public void exampleTest3() {
+        assertArrayEquals(new String[]{ "top", "middle", "bottom" },
+                Task41.fixTheMeerkat(new String[]{ "bottom", "middle", "top" }));
+    }
+
+    @Test
+    public void exampleTest4() {
+        assertArrayEquals(new String[]{ "upper legs", "torso", "lower legs" },
+                Task41.fixTheMeerkat(new String[]{ "lower legs", "torso", "upper legs" }));
+    }
+
+    @Test
+    public void exampleTest5() {
+        assertArrayEquals(new String[]{ "sky", "rainbow", "ground" },
+                Task41.fixTheMeerkat(new String[]{ "ground", "rainbow", "sky" }));
+    }
 }
