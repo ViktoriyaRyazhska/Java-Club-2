e5e04457c2bc50548d162b7380f30f3753eb67da
Shum616 <48323638+Shum616@users.noreply.github.com>
2021-08-31 21:28:07 +0300

    Task5 had been changed

--- Library/src/SCRIPTS/BASIC/MostPopular_LeastPopular_Book.sql
+++ Library/src/SCRIPTS/BASIC/MostPopular_LeastPopular_Book.sql
@@ -1,35 +1,18 @@
-CREATE FUNCTION dbo.MostPopularBook (@DateStart date, @DateFin date)
-	RETURN varchar(128)
-	AS
-	BEGIN
 
-	RETURN
-		(SELECT TOP (1) topBook
-		FROM 
-			(SELECT book.title as topBook, COUNT(history.BookID) as NUMB
-			FROM history JOIN book ON book.id = history.BookID 
-			WHERE @DateStart >= history.dateBorrowed AND
-			@DateFin <= history.dateBorrowed 
-			GROUP BY book.title
-			ORDER BY COUNT(history.BookID) DESC)
-	END;
+		SELECT book.title
+		FROM history JOIN book ON book.id = history.BookID
+        WHERE history.dateBorrowed <= '30-09-2001'
+          AND
+                '30-09-2001' <= history.dateBorrowed
+		GROUP BY book.title
+		ORDER BY COUNT(history.BookID) DESC
+        LIMIT 1;
 
---DROP FUNCTION dbo.MostPopularBook;
 
-CREATE FUNCTION dbo.LeastPopularBook (@DateStart date, @DateFin date)
-	RETURN varchar(128)
-	AS
-	BEGIN
-
-	RETURN
-		(SELECT TOP (1) bottomBook
-		FROM 
-			(SELECT book.title as bottomBook, COUNT(history.BookID) as NUMB
-			FROM history JOIN book ON book.id = history.BookID 
-			WHERE @DateStart >= history.dateBorrowed AND
-			@DateFin <= history.dateBorrowed 
-			GROUP BY book.title
-			ORDER BY COUNT(history.BookID))
-	END;
-
---DROP FUNCTION dbo.LeastPopularBook;
+	    SELECT book.title
+		FROM history JOIN book ON book.id = history.BookID
+		WHERE '30-09-2001' >= history.dateBorrowed AND
+                '30-09-2001' <= history.dateBorrowed
+		GROUP BY book.title
+		ORDER BY COUNT(history.BookID)
+        LIMIT 1;
