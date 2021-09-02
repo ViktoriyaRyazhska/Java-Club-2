f8a5e4a41a5850ebd3056e8eaff619251aeb5c77
Shum616 <48323638+Shum616@users.noreply.github.com>
2021-09-03 00:15:30 +0300

    Task11 is modified

--- Library/src/SCRIPTS/BASIC/AverageReadingTime.sql
+++ Library/src/SCRIPTS/BASIC/AverageReadingTime.sql
@@ -1,2 +1,8 @@
-SELECT AVG(DATEDIFF( history.dateReturned, history.dateBorrowed)) as Average_Reading_Time FROM history
-    WHERE history.dateReturned IS NOT NULL;
+SELECT AVG(DATEDIFF( book_order.CLOSED_AT, book_order.CREATED_AT)) as Average_Reading_Time,
+       book.title, book.description, book.publicationDate, book_copy.book_idbook, book_copy.book_status_idbook_status,
+        user.firstName, user.lastName
+FROM book_order
+         JOIN book ON book.idbook = book_order.book_copy_idbook_copy
+         JOIN book_copy ON book.idbook = book_copy.book_idbook
+         JOIN user ON book_order.user_idUser = user.idUser and book_order.user_lastName = user.lastName
+    WHERE book_order.CLOSED_AT IS NOT NULL;
