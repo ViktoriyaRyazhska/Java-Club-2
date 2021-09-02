8d7fbc7cec8b90c6f0453f182a82cc30e32af56e
Shum616 <48323638+Shum616@users.noreply.github.com>
2021-09-02 23:52:25 +0300

    Task5 is modified

--- Library/src/SCRIPTS/BASIC/MostPopular_LeastPopular_Book.sql
+++ Library/src/SCRIPTS/BASIC/MostPopular_LeastPopular_Book.sql
@@ -1,19 +1,17 @@
 
-		SELECT book.title
-		FROM history JOIN book ON book.id = history.BookID
-        WHERE history.dateBorrowed <= '30-09-2001'
-          AND
-                '30-09-2001' <= history.dateBorrowed
-		GROUP BY book.title
-		ORDER BY COUNT(history.BookID) DESC
-        LIMIT 1;
+SELECT book.title
+FROM book_order JOIN book ON book.idbook = book_order.book_copy_idbook_copy
+WHERE '30-09-2001' >= book_order.CREATED_AT AND
+        '30-09-2001' <= book_order.CLOSED_AT
+GROUP BY book.title
+ORDER BY COUNT(book_order.book_copy_idbook_copy) DESC
+LIMIT 1;
 
 
-	    SELECT book.title
-		FROM history JOIN book ON book.id = history.BookID
-		WHERE '30-09-2001' >= history.dateBorrowed AND
-                '30-09-2001' <= history.dateBorrowed
-		GROUP BY book.title
-		ORDER BY COUNT(history.BookID)
-        LIMIT 1;
-
+SELECT book.title
+FROM book_order JOIN book ON book.idbook = book_order.book_copy_idbook_copy
+WHERE '30-09-2001' >= book_order.CREATED_AT AND
+        '30-09-2001' <= book_order.CLOSED_AT
+GROUP BY book.title
+ORDER BY COUNT(book_order.book_copy_idbook_copy)
+LIMIT 1;
