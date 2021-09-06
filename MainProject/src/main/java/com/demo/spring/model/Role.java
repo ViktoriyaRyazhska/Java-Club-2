c16d9881859f4d0ccf68686270b0f002c41649da
Shum616 <48323638+Shum616@users.noreply.github.com>
2021-09-06 23:26:45 +0300

    Annotation for user_table is done and support enum Role is created

---
+++ MainProject/src/main/java/com/demo/spring/model/Role.java
@@ -0,0 +1,5 @@
+package com.demo.spring.model;
+
+public enum Role {
+    reader, manager
+}
---
+++ MainProject/src/main/java/com/demo/spring/model/user_table.java
@@ -0,0 +1,44 @@
+package com.demo.spring.model;
+
+import org.hibernate.annotations.Table;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+public class user_table {
+    @Id @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column
+    @Type(type="birthDate")
+    private Date birthDate;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "CREATED_AT")
+    private Timestamp CREATED_AT;
+
+    @Column(name = "CLOSED_AT")
+    private Timestamp CLOSED_AT;
+
+    @Column(name = "is_deleted")
+    private int is_deleted;
+
+}
