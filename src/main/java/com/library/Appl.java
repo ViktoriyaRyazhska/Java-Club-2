99c5dac05a5fa037eea60a7a18e33d3e27ae6972
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2021-09-10 21:14:11 +0300

    Setup project

---
+++ src/main/java/com/library/Appl.java
@@ -0,0 +1,14 @@
+package com.library;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+
+public class Appl {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/library/model/enums/Roles.java
@@ -0,0 +1,4 @@
+package com.library.model.enums;
+
+public enum Roles {
+}
